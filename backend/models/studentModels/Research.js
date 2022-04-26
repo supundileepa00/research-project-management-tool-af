aa9f98a64a7dbcea79452d056403090293ab9508
Ramith <89182652+ramithperera@users.noreply.github.com>
2022-04-27 00:10:00 +0530

    Created Module bro
    Too lazy to create the routes haha

---
+++ backend/models/studentModels/Research.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ResearchSchema = new Schema({
+  name: { String, require: true },
+  topic: { String, require: true },
+  document: { data: buffer, contentType: String },
+});
+
+const Research = mongoose.model("Research", ResearchSchema);
+module.exports = Research;
