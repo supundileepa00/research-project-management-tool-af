5faff682cc1cfbead653860f85d08da49bba2727
Ramith <89182652+ramithperera@users.noreply.github.com>
2022-04-27 22:37:36 +0530

    Model updated with values
    Forgot somethings to be done lol

--- backend/models/studentModels/Research.js
+++ backend/models/studentModels/Research.js
@@ -5,6 +5,8 @@ const Schema = mongoose.Schema;
 const ResearchSchema = new Schema({
   name: { String, require: true },
   topic: { String, require: true },
+  groupID: { String, require: true },
+  faculty: { String, require: true },
   document: { data: buffer, contentType: String },
 });
 
