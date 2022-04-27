7bfcba14406510f9eefbc1f3ccf3f9841414a0d2
Ramith <89182652+ramithperera@users.noreply.github.com>
2022-04-27 23:15:25 +0530

    Format the model

--- backend/models/studentModels/Research.js
+++ backend/models/studentModels/Research.js
@@ -3,11 +3,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ResearchSchema = new Schema({
-  name: { String, require: true },
-  topic: { String, require: true },
-  groupID: { String, require: true },
-  faculty: { String, require: true },
-  document: { data: buffer, contentType: String },
+  name: {
+    String,
+    require: true,
+  },
+  topic: {
+    String,
+    require: true,
+  },
+  groupID: {
+    String,
+    require: true,
+  },
+  faculty: {
+    String,
+    require: true,
+  },
+  document: {
+    data: buffer,
+    contentType: String,
+  },
 });
 
 const Research = mongoose.model("Research", ResearchSchema);
