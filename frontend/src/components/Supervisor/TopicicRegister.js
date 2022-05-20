fa293f4a463d18660c712651e335a7692ff3c723
san678 <sandeepalakshan456@gmail.com>
2022-05-20 23:24:43 +0530

    Update TopicicRegister.js

--- frontend/src/components/Supervisor/TopicicRegister.js
+++ frontend/src/components/Supervisor/TopicicRegister.js
@@ -9,142 +9,202 @@ import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import { useEffect, useState } from "react";
 
 //import { useEffect, useState } from "react";
 
 function TopicRegister() {
-  const [name, setName] = React.useState("");
-  const [registerId, setRegisterId] = React.useState("");
-  const [groupId, setGroupId] = React.useState("");
-  const [faculty, setFaculty] = React.useState("");
-  const [department, setDepartment] = React.useState("");
-  const [topic, setTopic] = React.useState("");
+  const [name, setName] = useState("");
+  const [regNo, setRegNo] = useState("");
+  const [groupID, setGroupID] = useState("");
+  const [topic, setTopic] = useState("");
+  const [faculty, setFaculty] = useState("");
+  const [department, setDepartment] = useState("");
+  const [field, setField] = useState("");
 
-  const [age, setAge] = React.useState("");
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
+  const handleFaculty = (event) => {
+    setFaculty(event.target.value);
   };
+
+  const handleDepartment = (event) => {
+    setDepartment(event.target.value);
+  };
+
+  const handleField = (event) => {
+    setField(event.target.value);
+  };
+
+  function sendData(e) {
+    e.preventDefault();
+    alert("Insert");
+
+    const newTopic = {
+      name,
+      regNo,
+      groupID,
+      faculty,
+      department,
+      field,
+      topic,
+    };
+  }
 
   return (
     <div>
       <h1>Topic Register</h1>
-
-      <div className="App">
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 2, width: "52ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField id="name" label="Name" variant="outlined" />
-        </Box>
-
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            id="registerNumber"
-            label="Register Number"
-            variant="outlined"
-          />
-          <input
-            type="text"
-            class="App"
-            id="registerNumber"
-            onChange={(e) => {
-              setRegisterId(e.target.value);
+      <form onSubmit={sendData}>
+        <div className="App">
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "52ch" },
             }}
-          />
-
-          <TextField id="groupId" label="Group Id" variant="outlined" />
-          <input
-            type="text"
-            class="App"
-            id="groupId"
-            onChange={(e) => {
-              setGroupId(e.target.value);
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="name"
+              label="Name"
+              variant="outlined"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+          </Box>
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "25ch" },
             }}
-          />
-        </Box>
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="regNo"
+              label="Register Number"
+              variant="outlined"
+              onChange={(e) => {
+                setRegNo(e.target.event);
+              }}
+            />
+            <TextField
+              id="groupID"
+              label="Group ID"
+              variant="outlined"
+              onChange={(e) => {
+                setGroupID(e.target.value);
+              }}
+            />
+          </Box>
 
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField id="faculty" label="Faculty" variant="outlined" />
-          <input
-            type="text"
-            class="App"
-            id="faculty"
-            onChange={(e) => {
-              setFaculty(e.target.value);
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "25ch" },
             }}
-          />
-          <TextField id="department" label="Department" variant="outlined" />
-          <input
-            type="text"
-            class="App"
-            id="department"
-            onChange={(e) => {
-              setDepartment(e.target.value);
+            noValidate
+            autoComplete="off"
+          >
+            <FormControl fullWidth>
+              <InputLabel id="faculty-select-label">Faculty</InputLabel>
+              <Select
+                labelId="faculty-select-label"
+                id="faculty-select"
+                value={faculty}
+                label="selectFaculty"
+                onChange={handleFaculty}
+              >
+                <MenuItem value={"Faculty of Computing"}>
+                  Faculty of Computing
+                </MenuItem>
+                <MenuItem value={"Faculty of Business"}>
+                  Faculty of Business
+                </MenuItem>
+                <MenuItem value={"Faculty of Engineering"}>
+                  Faculty of Engineering
+                </MenuItem>
+              </Select>
+            </FormControl>
+
+            <FormControl fullWidth>
+              <InputLabel id="department-select-label">Department</InputLabel>
+              <Select
+                labelId="department-select-label"
+                id="department-select"
+                value={department}
+                label="selectDepartment"
+                onChange={handleDepartment}
+              >
+                <MenuItem value={"IT/SE/CSNE/ISE/CS"}>
+                  IT/SE/CSNE/ISE/CS
+                </MenuItem>
+                <MenuItem value={"Business Management (SLIIT)"}>
+                  Business Management (SLIIT)
+                </MenuItem>
+                <MenuItem value={"Business Management (LJMU)"}>
+                  Business Management (LJMU)
+                </MenuItem>
+                <MenuItem value={"Business Management (QU)"}>
+                  Business Management (QU)
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "52ch" },
             }}
-          />
-        </Box>
+            noValidate
+            autoComplete="off"
+          >
+            <FormControl fullWidth>
+              <InputLabel id="field-select-label">Research Field</InputLabel>
+              <Select
+                labelId="field-select-label"
+                id="filed-select"
+                value={field}
+                label="selectField"
+                onChange={handleField}
+              >
+                <MenuItem value={"Computer Science"}>Computer Science</MenuItem>
+                <MenuItem value={"Computer History"}>Computer History</MenuItem>
+                <MenuItem value={"Business Opportunities"}>
+                  Business Opportunities
+                </MenuItem>
+                <MenuItem value={"Mechanical Engineering"}>
+                  Mechanical Engineering
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
 
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 2, width: "52ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">
-              Research Field
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={age}
-              label="Age"
-              onChange={handleChange}
-            >
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "52ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField id="topic" label="Topic" variant="outlined" />
-        </Box>
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "52ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="topic"
+              label="Topic"
+              variant="outlined"
+              onChange={(e) => {
+                setTopic(e.target.value);
+              }}
+            />
+          </Box>
+        </div>
 
         <center>
           <Button variant="contained">Submit</Button>
         </center>
-      </div>
+      </form>
     </div>
   );
 }
+
 export default TopicRegister;
