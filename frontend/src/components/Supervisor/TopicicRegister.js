cd33006a3e78d049f487e13a004fa62f71be74be
san678 <sandeepalakshan456@gmail.com>
2022-05-25 23:30:59 +0530

    update
    topic register page

--- frontend/src/components/Supervisor/TopicicRegister.js
+++ frontend/src/components/Supervisor/TopicicRegister.js
@@ -12,15 +12,16 @@ import Grid from "@mui/material/Grid";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-import { useEffect, useState } from "react";
-
 function TopicRegister() {
-const [name, setName] = React.useState("");
- const [registerId, setRegisterId] = React.useState("");
- const [groupId, setGroupId] = React.useState("");
- const [faculty, setFaculty] = React.useState("");
- const [department, setDepartment] = React.useState("");
- const [topic, setTopic] = React.useState("");
+  const [name, setName] = useState("");
+  const [regNo, setRegNo] = useState("");
+  //const [registerId, setRegisterId] = useState("");
+  const [groupID, setGroupID] = useState("");
+
+  const [faculty, setFaculty] = React.useState("");
+  const [department, setDepartment] = React.useState("");
+  const [field, setField] = React.useState("");
+  const [topic, setTopic] = useState("");
 
   const handleFaculty = (event) => {
     setFaculty(event.target.value);
@@ -61,54 +62,113 @@ const [name, setName] = React.useState("");
       <h1>Topic Register</h1>
 
       <div className="App">
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 2, width: "52ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField id="name" label="Name" variant="outlined" />
-         
-        </Box>
+        <form>
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 2, width: "52ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField id="name" label="Name" variant="outlined" />
+          </Box>
 
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            id="registerNumber"
-            label="Register Number"
-            variant="outlined"
-          />
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="registerNumber"
+              label="Register Number"
+              variant="outlined"
+            />
 
-          <TextField id="groupId" label="Group Id" variant="outlined" />
+            <TextField id="groupId" label="Group Id" variant="outlined" />
+          </Box>
 
-        </Box>
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <FormControl fullWidth>
+              <InputLabel id="faculty-select-label">Faculty</InputLabel>
+              <Select
+                labelId="faculty-select-label"
+                id="faculty-select"
+                value={faculty}
+                label="Faculty"
+                onChange={handleFaculty}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
 
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField id="faculty" label="Faculty" variant="outlined" />
-          
-          <TextField id="department" label="Department" variant="outlined" />
-         
-        </Box>
+            <FormControl fullWidth>
+              <InputLabel id="department-select-label">Department</InputLabel>
+              <Select
+                labelId="department-select-label"
+                id="department-select"
+                value={department}
+                label="Department"
+                onChange={handleDepartment}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
 
-        <center>
-          <Button variant="contained">Submit</Button>
-        </center>
-      </form>
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "52ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <FormControl fullWidth>
+              <InputLabel id="field-select-label">Research Field</InputLabel>
+              <Select
+                labelId="field-select-label"
+                id="field-select"
+                value={field}
+                label="Field"
+                onChange={handleField}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 2, width: "52ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField id="topic" label="Topic" variant="outlined" />
+          </Box>
+          <center>
+            <Button variant="contained">Submit</Button>
+          </center>
+        </form>
+      </div>
     </div>
   );
 }
